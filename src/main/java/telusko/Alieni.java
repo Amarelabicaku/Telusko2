728df5c5e0d6dab184d279e320427cdde0aa6f58
Amarela <amarelabicaku@gmail.com>
2022-09-15 15:32:04 -0700

    Initial commit

---
+++ src/main/java/telusko/Alieni.java
@@ -0,0 +1,31 @@
+package telusko;
+
+public class App {
+private int id;
+private String name;
+private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/telusko/Laptop1.java
@@ -0,0 +1,2 @@
+package telusko;public class Laptop1 {
+}
